f3ee2bb09a39b0353de42fb803917cb8468d2c7b
onichandame <zxinmyth@gmail.com>
2020-04-09 07:37:08 +0000

    line chart responsive

--- src/Chart/RealTimeLineChart.tsx
+++ src/Chart/RealTimeLineChart.tsx
@@ -1,5 +1,8 @@
 import React, { FC, ComponentProps } from 'react'
-import { makeStyles, Theme } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
+import {
+  Grid
+} from '@material-ui/core'
 import {
   LineChart,
   CartesianGrid,
@@ -18,22 +21,14 @@ type Props={
   y: string[];
 }
 
-const useStyles = makeStyles((theme: Theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
-    display: 'flex',
-    flexGrow: 1,
-    flexDirection: 'column',
-    backgroundColor: theme.palette.background.paper
+    height: '100%'
   },
   label: {
-    display: 'flex',
-    flexGrow: 0,
+    textAlign: 'center',
     fontWeight: 'bold',
-    justifyContent: 'center'
-  },
-  chart: {
-    display: 'flex',
-    flexGrow: 0
+    fontSize: '2rem'
   }
 }))
 
@@ -51,28 +46,30 @@ export const RealTimeLineChart: FC<Props> = ({
 }: Props) => {
   const styles = useStyles()
   return (
-    <div className={styles.root}>
-      <h2 className={styles.label}>
+    <Grid container spacing={0} className={styles.root}>
+      <Grid item xs={12} className={styles.label}>
         {label}
-      </h2>
-      <ResponsiveContainer className={styles.chart}>
-        <LineChart
-          data={data}
-          margin={{ top: 5, right: 30, left: 0, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="3 3"/>
-          <XAxis dataKey={x}/>
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          {
-            y.map((value, index) => (
-              <Line isAnimationActive={true} animationDuration={500} dot={false} key={value} type={'monotone'} dataKey={value} stroke={colors[index % colors.length]}/>
-            ))
-          }
-        </LineChart>
-      </ResponsiveContainer>
-    </div>
+      </Grid>
+      <Grid item xs={12}>
+        <ResponsiveContainer minHeight={'100px'}>
+          <LineChart
+            data={data}
+            margin={{ top: 5, right: 30, left: 0, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3"/>
+            <XAxis dataKey={x}/>
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            {
+              y.map((value, index) => (
+                <Line isAnimationActive={true} animationDuration={500} dot={false} key={value} type={'monotone'} dataKey={value} stroke={colors[index % colors.length]}/>
+              ))
+            }
+          </LineChart>
+        </ResponsiveContainer>
+      </Grid>
+    </Grid>
   )
 }
 export default RealTimeLineChart
