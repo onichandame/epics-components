612a024bc687dd08574f7b182f77300a14a9c95d
onichandame <zxinmyth@gmail.com>
2020-04-09 08:24:25 +0000

    added indicator group

---
+++ src/Indicator/IndicatorGroup.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from 'react'
+import {
+  Grid
+} from '@material-ui/core'
+
+import { generate } from 'randomstring'
+
+import BinaryIndicator from './BinaryIndicator'
+
+type Field={
+  value: boolean;
+  label: string;
+}
+
+type Props={
+  data: Field[];
+}
+
+export const IndicatorGroup: FC<Props> = ({ data }: Props) => {
+  return (
+    <Grid container spacing={2}>
+      {data.map(field => (
+        <Grid key={`indi-grp-${generate({ length: 10 })}`} item>
+          <BinaryIndicator {...field}/>
+        </Grid>
+      ))}
+    </Grid>
+  )
+}
+export default IndicatorGroup
