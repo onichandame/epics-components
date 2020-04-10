19e29230eb216f58dc4df687036edccd3c330ae1
onichandame <zxinmyth@gmail.com>
2020-04-10 06:56:24 +0000

    fixed test

--- src/Panel/ButtonPanel.test.tsx
+++ src/Panel/ButtonPanel.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import Subject, { ButtonPanelRow } from './ButtonPanel'
+import Subject from './ButtonPanel'
 
 describe('button panel', () => {
   test('renders without throw', () => {
@@ -12,9 +12,7 @@ describe('button panel', () => {
     const but = <button>hi</button>
     expect(shallow(
       <Subject>
-        <ButtonPanelRow>
-          {but}
-        </ButtonPanelRow>
+        {but}
       </Subject>
     ).contains(but)).toBeTruthy()
   })
@@ -23,43 +21,6 @@ describe('button panel', () => {
     const but1 = <button>hi</button>
     const but2 = <button>hello</button>
     const element = <Subject>{[but1, but2]}</Subject>
-    expect(shallow(element).contains(but1)).toBeTruthy()
-    expect(shallow(element).contains(but2)).toBeTruthy()
-  })
-
-  test('renders 4 button in 2 rows', () => {
-    const but1 = <button>hi</button>
-    const but2 = <button>hello</button>
-    const but3 = <button>ciao</button>
-    const but4 = <button>bonjour</button>
-    const element = (
-      <Subject>
-        <ButtonPanelRow>
-          {[but1, but2]}
-        </ButtonPanelRow>
-        <ButtonPanelRow>
-          {[but3, but4]}
-        </ButtonPanelRow>
-      </Subject>
-    )
-    expect(shallow(element).contains(but1)).toBeTruthy()
-    expect(shallow(element).contains(but2)).toBeTruthy()
-    expect(shallow(element).contains(but3)).toBeTruthy()
-    expect(shallow(element).contains(but4)).toBeTruthy()
-  })
-
-  test('renders 2 row where 1 is empty', () => {
-    const but1 = <button>hi</button>
-    const but2 = <button>hello</button>
-    const element = (
-      <Subject>
-        <ButtonPanelRow>
-        </ButtonPanelRow>
-        <ButtonPanelRow>
-          {[but1, but2]}
-        </ButtonPanelRow>
-      </Subject>
-    )
     expect(shallow(element).contains(but1)).toBeTruthy()
     expect(shallow(element).contains(but2)).toBeTruthy()
   })
